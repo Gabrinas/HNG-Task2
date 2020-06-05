123e3908380220ee872eacaa1da8c787074c9b96
Gabrinas <65027184+Gabrinas@users.noreply.github.com>
2020-06-05 12:14:28 +0100

    Create Gabriel_HNGi7-06269.js

---
+++ Gabriel_HNGi7-06269.js
@@ -0,0 +1,11 @@
+
+let Info = {
+
+  "Myname" : "Sobola Gabriel Oluwatobi",
+  "ID" : "HNGi7-06269",
+  "Email" : "sobolagabriel@gmail.com",
+  "Language" : "javascript"
+  
+  };
+
+console.log(`Hello World, this is ${Info.Myname} with ${Info.ID} using  ${Info.Language} for stage 2 task. ${Info.Email}`) 
